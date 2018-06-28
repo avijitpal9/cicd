3672529f06b0a3abe3ea765a41fc02e1e9188232
Avijit Pal <avijitpal.1987@gmail.com>
2018-06-28 19:45:19 +0530

    commit 1

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mywebapp',
+      version='0.1',
+      description='My Python Flask Application',
+      url='https://github.com/avijitpal9/cicd',
+      author='AvijitPal',
+      zip_safe=False)
